22689be6bebae683ea77540f97c692dacb7e7afe
frickiericker <frickiericker@gmail.com>
2017-05-21 04:14:54 +0900

    Make output format configurable via command-line option

--- main.go
+++ main.go
@@ -1,42 +1,61 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/frickiericker/acol/printers"
 	"github.com/frickiericker/acol/utils"
+	"github.com/mkideal/cli"
 )
 
+type argT struct {
+	cli.Helper
+	RowMajor bool `cli:"r" usage:"use row-major ordering"`
+	Spacing  int  `cli:"s" usage:"space between columns" dft:"2"`
+}
+
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintln(os.Stderr, "error:", err)
-		os.Exit(1)
-	}
+	cli.Run(new(argT), func(ctx *cli.Context) error {
+		argv := ctx.Argv().(*argT)
+		in := os.Stdin
+		out := os.Stdout
+		printer := makePrinter(argv)
+		return run(in, out, printer)
+	})
 }
 
-func run() error {
-	items, err := utils.ReadLines(os.Stdin)
-	if err != nil {
-		return fmt.Errorf("reading from standard input: %s", err)
-	}
-
-	printer, err := makePrinter()
-	if err != nil {
-		return fmt.Errorf("creating printer: %s", err)
-	}
-
-	cells := printers.MakeCells(items)
-	return printer.Print(os.Stdout, cells)
-}
-
-func makePrinter() (printers.Interface, error) {
+func makePrinter(argv *argT) printers.Interface {
 	width, err := getTerminalWidth()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "warning:", err)
 		width = 0
 	}
-	spacing := 2
-	printer := printers.NewDenseColumnMajor(width, spacing)
-	return printer, nil
+	spacing := argv.Spacing
+
+	if argv.RowMajor {
+		return printers.NewDenseRowMajor(width, spacing)
+	}
+	return printers.NewDenseColumnMajor(width, spacing)
+}
+
+func run(in io.Reader, out io.Writer, printer printers.Interface) error {
+	items, err := utils.ReadLines(in)
+	if err != nil {
+		return fmt.Errorf("reading from input: %s", err)
+	}
+	cells := printers.MakeCells(items)
+	return printer.Print(out, cells)
+}
+
+/*
+Validate validates command-line arguments.
+*/
+func (argv *argT) Validate(ctx *cli.Context) error {
+	if argv.Spacing < 0 {
+		return errors.New("space between columns cannot be negative")
+	}
+	return nil
 }
