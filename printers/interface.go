1be88e8c72fc4192d491cae43bf34378387b009c
frickiericker <frickiericker@gmail.com>
2017-05-21 03:03:37 +0900

    Document printers.Interface
    Made to start with Interface... following golint.

--- printers/interface.go
+++ printers/interface.go
@@ -5,8 +5,8 @@ import (
 )
 
 /*
-Common interface of printers. A printer writes given cells (strings with width
-information) to a Writer in a specific format.
+Interface defines functions provided by printers. A printer writes given cells
+(strings with width information) to a Writer in a specific format.
 */
 type Interface interface {
 	Print(out io.Writer, cells []Cell)
